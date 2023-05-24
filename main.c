20e0b49df23b900bc17eb4a8680493a927750cbb
Keabetswe14 <londiwedlamini857@gmail.com>
2023-05-24 05:26:34 -0700

    simple_shell

--- main.c
+++ main.c
@@ -1,18 +1,71 @@
 #include "main.h"
+int main(int ac, char **argv)
+{
+char *prompt = "(Sshell) $ ";
+char *lineptr = NULL, *lineptr_copy = NULL;
+size_t n = 0;
+ssize_t nchars_read;
+const char *delim = " \n";
+int num_tokens = 0;
+char *token;
+int i;
+/* declaring void variables */
+(void)ac;
 
-int main(int ac, char **argv){
-	char *prompt = "(Sshell) $ ";
-	char *lineptr;
-	size_t n = 0;
+/* Create a loop for the shell's prompt */
+    while (1) 
+{
+        printf("%s", prompt);
+        nchars_read = getline(&lineptr, &n, stdin);
+        /* check if the getline function failed or reached EOF or user use CTRL + D */ 
+        if (nchars_read == -1){
+            printf("Exiting shell....\n");
+            return (-1);
+        }
 
-	/* declaring void variables */
-	(void)ac; (void)argv;
+        /* allocate space for a copy of the lineptr */
+        lineptr_copy = malloc(sizeof(char) * nchars_read);
+        if (lineptr_copy== NULL){
+            perror("tsh: memory allocation error");
+            return (-1);
+        }
+        /* copy lineptr to lineptr_copy */
+        strcpy(lineptr_copy, lineptr);
 
-	printf("%s", prompt);
-	getline(&lineptr, &n, stdin);
-	printf("%s\n", lineptr);
+        /********** split the string (lineptr) into an array of words ********/
+        /* calculate the total number of tokens */
+        token = strtok(lineptr, delim);
 
-	free(lineptr);
-	return (0);
+        while (token != NULL){
+            num_tokens++;
+            token = strtok(NULL, delim);
+        }
+        num_tokens++;
+
+        /* Allocate space to hold the array of strings */
+        argv = malloc(sizeof(char *) * num_tokens);
+
+        /* Store each token in the argv array */
+        token = strtok(lineptr_copy, delim);
+
+        for (i = 0; token != NULL; i++){
+            argv[i] = malloc(sizeof(char) * strlen(token));
+            strcpy(argv[i], token);
+
+            token = strtok(NULL, delim);
+        }
+        argv[i] = NULL;
+
+        /* execute the command */
+        execmd(argv);
+
+    } 
+
+
+    /* free up allocated memory */ 
+    free(lineptr_copy);
+    free(lineptr);
+
+    return (0);
 }
 
